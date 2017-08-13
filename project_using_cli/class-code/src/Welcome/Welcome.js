2077e054e92ff52c790f36c4c84f036727705caa
Yousuf Qutubuddin <usuf52@gmail.com>
2017-08-13 13:14:17 +0500

    class code 13 Aug 2017

---
+++ project_using_cli/class-code/src/Welcome/Welcome.js
@@ -0,0 +1,15 @@
+import React, {
+    Component
+} from 'react';
+
+class Welcome extends React.Component {
+
+    render() {
+        return ( <
+            h1 > Welcome Component < /h1>
+        )
+    }
+
+}
+
+export default Welcome;
